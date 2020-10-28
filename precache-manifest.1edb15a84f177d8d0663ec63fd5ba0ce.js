53d3ca71ad1ac6446fa476403220ef99437e71e3
Bartek <WonderfulLifeVision@gmail.com>
2020-10-28 17:22:05 +0100

    Updates

---
+++ precache-manifest.1edb15a84f177d8d0663ec63fd5ba0ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ab63a2fac9dfbde14f7563e558a3eb",
+    "url": "/pokemon-finder/index.html"
+  },
+  {
+    "revision": "866ef7a91999fb0a0965",
+    "url": "/pokemon-finder/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "c27126bab3474bf0165e",
+    "url": "/pokemon-finder/static/css/main.c422e45c.chunk.css"
+  },
+  {
+    "revision": "866ef7a91999fb0a0965",
+    "url": "/pokemon-finder/static/js/2.6c036fd4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokemon-finder/static/js/2.6c036fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c27126bab3474bf0165e",
+    "url": "/pokemon-finder/static/js/main.bca8e07d.chunk.js"
+  },
+  {
+    "revision": "0654723a3d9e0f22c962",
+    "url": "/pokemon-finder/static/js/runtime-main.8e53188b.js"
+  },
+  {
+    "revision": "b73b3265392c355b014f9f565d94aad1",
+    "url": "/pokemon-finder/static/media/pokemon-logo.b73b3265.png"
+  }
+]);
